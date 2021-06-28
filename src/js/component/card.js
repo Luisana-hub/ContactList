65a2acc2e8e702501dece7cc43decd7eae0c63e7
Luisana Viamonte <luisanaviamonte@gmail.com>
2021-06-28 13:44:23 -0400

    git commit

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -4,8 +4,29 @@ const Card = () => {
 	return (
 		<div className="mt-2">
 			<div className="container">
-				<h4>Contact</h4>
-				<h4>Contact2</h4>
+				<div className="card mb-3" style="max-width: 540px;">
+					<div className="row no-gutters">
+						<div className="col-md-4">
+							<img src="..." alt="..."></img>
+						</div>
+						<div className="col-md-8">
+							<div className="card-body">
+								<h5 className="card-title">Card title</h5>
+								<p className="card-text">
+									This is a wider card with supporting text
+									below as a natural lead-in to additional
+									content. This content is a little bit
+									longer.
+								</p>
+								<p className="card-text">
+									<small className="text-muted">
+										Last updated 3 mins ago
+									</small>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
