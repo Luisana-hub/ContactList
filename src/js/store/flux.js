4376f335d2c46ab62d280a03727f8d8424d2482f
Luisana Viamonte <luisanaviamonte@gmail.com>
2021-07-03 15:24:38 -0400

    git commit

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -31,9 +31,33 @@ const getState = ({ getStore, getActions, setStore }) => {
 					}) // eslint-disable-next-line no-console
 					.catch(error => console.log(error));
 			},
-			fetchAddContact: (fullName, phone, address, email) => {
+			fetchAddContact: (fullName, email, address, phone) => {
 				fetch("https://assets.breatheco.de/apis/fake/contact/", {
 					method: "POST",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify({
+						full_name: fullName,
+						email: email,
+						agenda_slug: "agenda_monalisa",
+						address: address,
+						phone: phone
+					})
+				}).then(() => {
+					fetch(
+						"https://assets.breatheco.de/apis/fake/contact/agenda/agenda_monalisa"
+					)
+						.then(response => response.json())
+						.then(data => {
+							// eslint-disable-next-line no-console
+							console.log("data", data),
+								setStore({ listContact: data });
+						}) // eslint-disable-next-line no-console
+						.catch(error => console.log(error));
+				});
+			},
+			fechtEditContact: (id, fullName, email, address, phone) => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/" + id, {
+					method: "PUT",
 					headers: { "Content-type": "application/json" },
 					body: JSON.stringify({
 						full_name: fullName,
--- src/js/views/edit.js
+++ src/js/views/edit.js
@@ -3,19 +3,88 @@ import Formulario from "../component/formulario";
 import { Link } from "react-router-dom";
 
 const Edit = () => {
+	const { store, actions } = useContext(Context);
+	const [fullName, setFullName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [address, setAddress] = useState("");
+
+	const onSubmit = e => {
+		e.preventDefault();
+		// eslint-disable-next-line no-console
+		console.log("se ejecuta el enter");
+	};
 	return (
 		<div className="mt-5">
 			<div className="row justify-content-center">
 				<h2>Edit contact</h2>
 			</div>
 			<div className="container">
-				<form>
-					<p1>Formulario</p1>
+				<form onSubmit={onSubmit}>
+					<div className="container">
+						<div className="form-group">
+							<label htmlFor="formGroupExampleInput">
+								Full Name
+							</label>
+							<input
+								type="text"
+								className="form-control"
+								id="formGroupExampleInput2"
+								placeholder="Full Name"
+								onChange={e =>
+									setFullName(e.target.value)
+								}></input>
+						</div>
+						<div className="form-group">
+							<label htmlFor="inputEmail4">Email</label>
+							<input
+								type="email"
+								className="form-control"
+								id="inputEmail4"
+								placeholder="Enter email"
+								onChange={e =>
+									setEmail(e.target.value)
+								}></input>
+						</div>
+						<div className="form-group">
+							<label htmlFor="formGroupExampleInput">Phone</label>
+							<input
+								type="text"
+								className="form-control"
+								id="formGroupExampleInput"
+								placeholder="Enter phone"
+								onChange={e =>
+									setPhone(e.target.value)
+								}></input>
+						</div>
+						<div className="form-group">
+							<label htmlFor="formGroupExampleInput">
+								Address
+							</label>
+							<input
+								type="text"
+								className="form-control"
+								id="formGroupExampleInput"
+								placeholder="Enter address"
+								onChange={e =>
+									setAddress(e.target.value)
+								}></input>
+						</div>
+					</div>
 					<div className="row">
 						<div className="col-12">
 							<button
 								type="submit"
-								className="btn btn-primary form-control">
+								className="btn btn-primary form-control"
+								onClick={() => {
+									actions.fetchEditContact(
+										id,
+										fullName,
+										email,
+										address,
+										phone
+									);
+								}}>
 								SAVE
 							</button>
 						</div>
